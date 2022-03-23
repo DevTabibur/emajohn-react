f6e054563560cdb78878298731f636d94fd4d9ee
DevTabibur <tobiburrohman2@gmail.com>
2022-03-23 11:00:58 +0600

    product component and data load done

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './Products.css'
 
 const Products = (props) => {
-    console.log(props.product);
     const {name, img, price, ratings, seller} = props.product;
     return (
         <div className='product'>
